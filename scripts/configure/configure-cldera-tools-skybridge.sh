2d5ec9689da68acf3cb67e433403e57b48ef60f3
Graham Harper <gbharpe@sandia.gov>
2022-10-18 16:02:46 -0600

    Touch up the scripts directory slightly, and add the env_mach_specific-skybridge

--- scripts/configure/configure-cldera-tools-skybridge.sh
+++ scripts/configure/configure-cldera-tools-skybridge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-SRC_DIR=/ascldap/users/gbharpe/Programming/cpp/CLDERA/cldera-tools
-INSTALL_DIR=/ascldap/users/gbharpe/Programming/cpp/CLDERA/cldera-tools-build
+SRC_DIR=/ascldap/users/gbharpe/hpc_home/CLDERA/cldera-tools-source
+INSTALL_DIR=/ascldap/users/gbharpe/hpc_home/CLDERA/cldera-tools-build
+
  
 rm -rf CMakeFiles
 rm -f  CMakeCache.txt
@@ -11,7 +12,8 @@ cmake \
   -D CMAKE_C_COMPILER:STRING=mpicc            \
   -D CMAKE_CXX_COMPILER:STRING=mpicxx         \
   -D CMAKE_Fortran_COMPILER:STRING=mpifort    \
-  -D CMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR} \
+  -D CMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}/release \
+  -D CLDERA_PNETCDF_PATH:PATH=${SEMS_NETCDF_ROOT}  \
   -D CLDERA_ENABLE_TESTS:BOOL=ON              \
   -D CLDERA_ENABLE_PROFILING_TOOL:BOOL=ON     \
   -D CLDERA_TESTS_MAX_RANKS:STRING=4          \

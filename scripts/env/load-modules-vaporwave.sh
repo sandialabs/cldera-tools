c41aa5f6edaa9fbce59efa3284c86a1cbdafa927
Graham Bennett Harper <gbharpe@sandia.gov>
2023-03-13 23:55:45 +0000

    Fix CI modules

--- scripts/env/load-modules-vaporwave.sh
+++ scripts/env/load-modules-vaporwave.sh
@@ -4,9 +4,8 @@
 module purge
 source /projects/sems/modulefiles/utils/sems-archive-modules-init.sh
 module load acme-env
-module load sems-archive-git/2.10.1
+module load sems-archive-git
 module load sems-archive-cmake/3.19.1
 module load acme-gcc/8.1.0
-module load acme-openmpi/2.1.5
-module load acme-hdf5/1.12.0/acme
+module load acme-openmpi/4.1.4
 module load acme-netcdf/4.7.4/acme

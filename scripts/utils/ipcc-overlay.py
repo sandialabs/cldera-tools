792790ba8f839a1010b8cc8ea297a9b987674d56
Luca Bertagna <lbertag@sandia.gov>
2023-06-12 11:28:32 -0600

    Factor out 'expect' python utility in utils.py file

--- scripts/utils/ipcc-overlay.py
+++ scripts/utils/ipcc-overlay.py
@@ -9,25 +9,9 @@ import matplotlib.animation as animation
 import matplotlib.patheffects as pe
 import matplotlib.pyplot as plt
 import ssl
+from utils import expect
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
-###############################################################################
-def expect(condition, error_msg, exc_type=SystemExit, error_prefix="ERROR:"):
-###############################################################################
-    """
-    Similar to assert except doesn't generate an ugly stacktrace. Useful for
-    checking user error, not programming error.
-
-    >>> expect(True, "error1")
-    >>> expect(False, "error2")
-    Traceback (most recent call last):
-        ...
-    SystemExit: ERROR: error2
-    """
-    if not condition:
-        msg = error_prefix + " " + error_msg
-        raise exc_type(msg)
 
 ###############################################################################
 def parse_command_line(args, description):

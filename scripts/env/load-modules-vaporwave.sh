5156e5f6cfb655628f384b5ade5e9fd72b55e0df
Luca Bertagna <lbertag@sandia.gov>
2024-02-19 14:06:54 -0700

    Extend gitlab ci to work on both vaporwave or luca's workstation

--- scripts/env/load-modules-vaporwave.sh
+++ scripts/env/load-modules-vaporwave.sh
@@ -9,3 +9,6 @@ module load sems-archive-cmake/3.19.1
 module load acme-gcc/8.1.0
 module load acme-openmpi/4.1.4
 module load acme-netcdf/4.7.4/acme
+
+# The configure-cldera-tools-runner.sh expects PNETCDF_ROOT
+export PNETCDF_ROOT=${SEMS_NETCDF_ROOT}
